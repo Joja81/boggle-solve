91a3e7e30ed6c73576eea9a18cad8b63d11b5924
Joshua Smee <smeejosh81@gmail.com>
2022-05-01 00:01:46 +1000

    Added bloggle board to show solutions

--- boggle/functions/solveAvl.js
+++ boggle/functions/solveAvl.js
@@ -1,108 +1,107 @@
 export const solveAvl = (avl, board) => {
+  let foundWords = [];
 
-    let foundWords = new Set()
+  for (let x = 0; x < board.length; x++) {
+    for (let y = 0; y < board.length; y++) {
+      let used = [[x, y]];
+      exploreAvl(used, avl[board[x][y]], board, foundWords);
+    }
+  }
 
-    for(let x = 0; x < board.length; x++){
-        for(let y = 0; y < board.length; y++){
-            let used = [[x,y]];
-            exploreAvl(used, avl[board[x][y]], board, foundWords);
+  foundWords.sort(function (first, second) {
+    return first.word.localeCompare(second.word);
+  });
+
+  return foundWords;
+};
+
+function exploreAvl(used, avl, board, foundWords) {
+  if ("word" in avl) {
+    calculateWord(used, board, foundWords);
+  }
+
+  let x = used[used.length - 1][0];
+  let y = used[used.length - 1][1];
+
+  for (let xChange = -1; xChange < 2; xChange++) {
+    for (let yChange = -1; yChange < 2; yChange++) {
+      let newPlace = [x + xChange, y + yChange];
+
+      if (validLocation(newPlace, board.length)) {
+        if (
+          visitedLocation(used, newPlace) &&
+          board[newPlace[0]][newPlace[1]] in avl
+        ) {
+          let newUsed = used.slice();
+          newUsed.push(newPlace);
+          exploreAvl(
+            newUsed,
+            avl[board[newPlace[0]][newPlace[1]]],
+            board,
+            foundWords
+          );
+        }
+      }
+    }
+  }
+}
+
+function validLocation(location, size) {
+  if (location[0] >= size || location[0] < 0) {
+    return false;
+  }
+
+  if (location[1] >= size || location[1] < 0) {
+    return false;
+  }
+  return true;
+}
+
+function visitedLocation(used, newPlace) {
+  let valid = true;
+
+  used.forEach((element) => {
+    if (element[0] == newPlace[0] && element[1] == newPlace[1]) {
+      valid = false;
+    }
+  });
+  return valid;
+}
+
+function calculateWord(used, board, foundWords) {
+  let word = "";
+
+  used.forEach((element) => {
+    word += board[element[0]][element[1]];
+  });
+
+  if (word.length >= 3) {
+    word = capataliseWord(word);
+
+    if (!wordFound(foundWords, word)) {
+      foundWords.push({ word: word, used: used });
+    }
+  }
+}
+
+function capataliseWord(word) {
+  let arr = word.split("");
+
+  arr[0] = arr[0].charAt(0).toUpperCase();
+
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toLowerCase();
+  }
+
+  return arr.join("");
+}
+
+function wordFound(foundWords, word){
+    for (const currWord of foundWords){
+        if (currWord.word == word){
+            return true;
         }
     }
 
-    
-    let solutions = computeSolutions(foundWords)
-
-    solutions.sort()
-
-    return solutions
-}
-
-function exploreAvl(used, avl, board, foundWords){
-
-    if ("word" in avl){
-        calculateWord(used, board, foundWords);
-    }
-
-    let x = used[used.length - 1][0]
-    let y = used[used.length - 1][1]
-
-
-    for(let xChange = -1; xChange < 2; xChange++){
-        for(let yChange = -1; yChange < 2; yChange++){
-            let newPlace = [x+xChange, y+yChange];
-
-            if (validLocation(newPlace, board.length)){
-                if (visitedLocation(used, newPlace) && (board[newPlace[0]][newPlace[1]] in avl)){
-                    let newUsed = used.slice();
-                    newUsed.push(newPlace);
-                    exploreAvl(newUsed, avl[board[newPlace[0]][newPlace[1]]], board, foundWords)
-                }
-            }
-
-        }
-    }
-
-}
-
-function validLocation(location, size){
-    if(location[0] >= size || location[0] < 0){
-        return false;
-    }
-
-    if(location[1] >= size || location[1] < 0){
-        return false
-    }
-    return true
-}
-
-function visitedLocation(used, newPlace){
-    let valid = true;
-
-    used.forEach(element => {
-
-        if(element[0] == newPlace[0] && element[1] == newPlace[1]){
-            valid = false;
-        }
-    });
-    return valid;
-}
-
-function calculateWord(used, board, foundWords){
-    let word = ""
-    
-    used.forEach(element => {
-        word += board[element[0]][element[1]]
-    });
-
-    if(word.length >= 3){
-
-        word = capataliseWord(word)
-
-        foundWords.add(word)
-    }
-
-}
-
-function capataliseWord(word){
-
-    let arr = word.split("") 
-
-    arr[0] = arr[0].charAt(0).toUpperCase();
-
-    for(let i = 1; i < arr.length; i++){
-        arr[i] = arr[i].charAt(0).toLowerCase();
-    }
-
-    return arr.join("");
-}
-
-function computeSolutions(foundWords){
-
-    let arr = []
-
-    for (var it = foundWords.values(), val =null; val=it.next().value; ){
-        arr.push(val)
-    }
-
-    return arr
+    return false;
 }
--- boggle/pages/solutions.js
+++ boggle/pages/solutions.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useContext, useState } from "react";
+import { UserContext } from "../lib/context";
 
 export default function Solutions() {
   const router = useRouter();
-  const query = router.query;
+
+  const {solutions, letters} = useContext(UserContext)
 
   return (
     <body>
-      {"solutions" in query ? (
-        <DisplaySolutions solutions={query["solutions"]} />
+      {solutions?  (
+        <DisplaySolutions solutions={solutions} board={letters} />
       ) : (
         <h1>Please enter a boggle board first</h1>
       )}
@@ -16,11 +19,17 @@ export default function Solutions() {
   );
 }
 
-function DisplaySolutions({ solutions }) {
+function DisplaySolutions({ solutions, board }) {
+
+  
+
   let itemList = [];
 
+  const [used, setUsed] = useState([])
+
+
   for (let i = 0; i < solutions.length; i++) {
-    itemList.push(<Solution solution={solutions[i]} key={"solution" + i} />);
+    itemList.push(<Solution solution={solutions[i]} key={"solution" + i} setUsed={setUsed}/>);
   }
 
   return <>
@@ -28,6 +37,7 @@ function DisplaySolutions({ solutions }) {
       <div className="split">
         {itemList}
       </div>
+      <Board board={board} used={used}/>
       <Link href={"/"} passHref>
       <button>
         Back
@@ -36,6 +46,78 @@ function DisplaySolutions({ solutions }) {
     </>
 }
 
-function Solution({ solution }) {
-  return <div>{solution}</div>;
+function Solution({ solution, setUsed }) {
+
+  const onClick = () => {
+    console.log("selecting");
+    setUsed(solution['used'])
+  }
+
+  return <div onClick={onClick}>{solution['word']}</div>;
+}
+
+function Board({board, used}){
+
+  let rowList = []
+  
+  for (let row = 0; row < board.length; row++){
+    rowList.push(<Row row={board[row]} rowNum = {row} used={used}/>)
+  }
+
+  return <div >
+    {rowList}
+  </div>
+}
+
+function Row({row, rowNum, used}){
+
+  let letterList = []
+  for (let letter = 0; letter < row.length; letter++){
+    letterList.push(<Letter letter={row[letter]} rowNum={rowNum} letterNum = {letter} used={used}/>)
+  }
+
+  return <div>
+    {letterList}
+  </div>
+}
+
+function Letter({letter, rowNum, letterNum, used}){
+
+  let selected = isSelected(rowNum, letterNum, used);
+
+  let className = "column square";
+
+  if (selected == 1){
+    className += " selected"
+  } else if (selected == 2){
+    className += " first"
+  }
+
+
+  return <div className={className}>
+    {letter}
+  </div>
+}
+
+
+function isSelected(rowNum, letterNum, used){
+
+  if (used.length < 1){
+    return 0
+  }
+
+  let curr = used[0]
+
+  if (curr[0] == rowNum && curr[1] == letterNum){
+    return 2;
+  }
+
+  for (let i = 1; i < used.length; i++){
+    curr = used[i]
+    if (curr[0] == rowNum && curr[1] == letterNum ){
+      return 1;
+    }
+  }
+
+  return 0
 }
